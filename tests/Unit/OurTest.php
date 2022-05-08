f8cc35d6d19d0a2cbb059d57ea04ef8b86a5cf96
Lebedenko Nikolay <totx@ya.ru>
2022-05-08 16:11:16 +0300

    improvements

--- tests/Unit/OurTest.php
+++ tests/Unit/OurTest.php
@@ -10,15 +10,21 @@ use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
+use YaPro\Helper\JsonHelper;
 use YaPro\SymfonySerializerUnderstanding\Tests\Model\DollModel;
 use YaPro\SymfonySerializerUnderstanding\Tests\Model\KenModel;
 
 class OurTest extends TestCase
 {
+    private static JsonHelper $jsonHelper;
     protected static SerializerInterface $serializer;
 
     public static function setUpBeforeClass(): void
     {
+        self::$jsonHelper = new JsonHelper();
+
+        // https://github.com/symfony/framework-bundle/blob/5.4/Resources/config/serializer.php
+        // https://github.com/symfony/symfony/issues/35554
         $encoders = [new XmlEncoder(), new JsonEncoder()];
         $normalizers = [new ObjectNormalizer()];
 
@@ -29,6 +35,17 @@ class OurTest extends TestCase
     {
     }
 
+    private function assertJsons(string $left, string $right)
+    {
+        // удаляем переносы строк и пробелы между именами полей и значениями, но не в значениях
+        $leftAsArray = self::$jsonHelper->jsonDecode($left, true);
+        $rightAsArray = self::$jsonHelper->jsonDecode($right, true);
+        $this->assertSame(
+            $leftAsArray,
+            $rightAsArray
+        );
+    }
+
     public function test1()
     {
         $kids = [
@@ -37,6 +54,23 @@ class OurTest extends TestCase
         ];
         $wife = new DollModel('Barbie');
         $result = self::$serializer->serialize(new KenModel('Ken', $wife, $kids, 'Doe', 'Moscow'), 'json');
-        $this->assertSame($result, '{"wife":{"name":"Barbie"},"kids":[{"name":"Todd"},{"name":"Stacie"}],"surname":"Doe","city":"Moscow","name":"Ken"}');
+        $this->assertJsons($result, '
+{
+    "wife": {
+        "name": "Barbie"
+    },
+    "kids": [
+        {
+            "name": "Todd"
+        },
+        {
+            "name": "Stacie"
+        }
+    ],
+    "surname": "Doe",
+    "city": "Moscow",
+    "name": "Ken"
+}
+        ');
     }
 }
