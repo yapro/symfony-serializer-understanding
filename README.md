8edb0df75ce1253e2ab0f8d5993110ede1ba0097
Lebedenko Nikolay <totx@ya.ru>
2022-05-14 13:54:06 +0300

    Added reverse serialization

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Так вышло, что документация Symfony Serializer Component в некоторых местах не является достаточно детальной, поэтому
 было решено рассмотреть основные ситуации в файле [tests/Unit/OurTest.php](tests/Unit/OurTest.php)
 
+Например Symfony Serializer Component (в рамках serializer/Normalizer/MimeMessageNormalizer.php) от зависит symfony/mime
+(а именно от Symfony\Component\Mime\Header\Headers), при этом ни в symfony/serializer-pack ни в symfony/serializer об
+этом не упоминается, как по мне, так это странно.
+
+Еще Symfony Serializer Component не имеет фабрики по созданию serializer-а в дефолтном состоянии, поэтому мы в
+[tests/Unit/OurTest.php](tests/Unit/OurTest.php) создаем serializer так, как это делает [бандл](https://github.com/symfony/framework-bundle/blob/5.4/Resources/config/serializer.php).
+
 ![lib tests](https://github.com/yapro/symfony-serializer-understanding/actions/workflows/main.yml/badge.svg)
 
 Build
@@ -30,7 +37,7 @@ XDEBUG_SESSION=common \
 XDEBUG_MODE=debug \
 XDEBUG_CONFIG="max_nesting_level=200 client_port=9003 client_host=host.docker.internal" \
 vendor/bin/phpunit  --cache-result-file=/tmp/phpunit.cache -v --stderr --stop-on-incomplete --stop-on-defect \
---stop-on-failure --stop-on-warning --fail-on-warning --stop-on-risky --fail-on-risky --testsuite=Functional
+--stop-on-failure --stop-on-warning --fail-on-warning --stop-on-risky --fail-on-risky --testsuite=Unit
 ```
 
 Cs-Fixer: fix code
--- tests/Unit/OurTest.php
+++ tests/Unit/OurTest.php
@@ -5,17 +5,44 @@ declare(strict_types=1);
 namespace YaPro\SymfonySerializerUnderstanding\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use stdClass;
+use Symfony\Component\Mime\MimeTypes;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Symfony\Component\PropertyAccess\PropertyAccessorBuilder;
+use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
+use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
+use Symfony\Component\PropertyInfo\Extractor\SerializerExtractor;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractor;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Mapping\ClassDiscriminatorFromClassMetadata;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\LoaderChain;
+use Symfony\Component\Serializer\NameConverter\MetadataAwareNameConverter;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
+use Symfony\Component\Serializer\Normalizer\ConstraintViolationListNormalizer;
+use Symfony\Component\Serializer\Normalizer\DataUriNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateIntervalNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeZoneNormalizer;
+use Symfony\Component\Serializer\Normalizer\FormErrorNormalizer;
+use Symfony\Component\Serializer\Normalizer\JsonSerializableNormalizer;
+use Symfony\Component\Serializer\Normalizer\MimeMessageNormalizer;
+use Symfony\Component\Serializer\Normalizer\ProblemNormalizer;
+use Symfony\Component\Serializer\Normalizer\PropertyNormalizer;
+use Symfony\Component\Serializer\Normalizer\UidNormalizer;
+use Symfony\Component\Serializer\Normalizer\UnwrappingDenormalizer;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use YaPro\Helper\JsonHelper;
 use YaPro\SymfonySerializerUnderstanding\Tests\Model\BarbieModel;
 use YaPro\SymfonySerializerUnderstanding\Tests\Model\DollModel;
 use YaPro\SymfonySerializerUnderstanding\Tests\Model\KenModel;
+
+use function json_decode;
 
 class OurTest extends TestCase
 {
@@ -26,10 +53,45 @@ class OurTest extends TestCase
     {
         self::$jsonHelper = new JsonHelper();
 
+        // https://github.com/symfony/symfony/issues/35554 so we need to implement the
         // https://github.com/symfony/framework-bundle/blob/5.4/Resources/config/serializer.php
-        // https://github.com/symfony/symfony/issues/35554
+
+        $reflectionExtractor = new ReflectionExtractor();
+        $phpDocExtractor = new PhpDocExtractor();
+
+        $LoaderChain = new LoaderChain([]);
+        $ClassMetadataFactory = new ClassMetadataFactory($LoaderChain);
+        $ClassDiscriminatorFromClassMetadata = new ClassDiscriminatorFromClassMetadata($ClassMetadataFactory);
+        $MetadataAwareNameConverter = new MetadataAwareNameConverter($ClassMetadataFactory);
+        // $SerializerExtractor = new SerializerExtractor($ClassMetadataFactory);
+        $ConstraintViolationListNormalizer = new ConstraintViolationListNormalizer([], $MetadataAwareNameConverter);
+        $PropertyInfoExtractor = new PropertyInfoExtractor(
+            [$reflectionExtractor],
+            [$phpDocExtractor, $reflectionExtractor],
+            [$phpDocExtractor],
+            [$reflectionExtractor],
+            [$reflectionExtractor]
+        );
+        $PropertyNormalizer = new PropertyNormalizer($ClassMetadataFactory, $MetadataAwareNameConverter, $PropertyInfoExtractor, $ClassDiscriminatorFromClassMetadata);
+        $PropertyAccessor = new PropertyAccessor();
+
         $encoders = [new XmlEncoder(), new JsonEncoder()];
-        $normalizers = [new ObjectNormalizer()];
+        $normalizers = [
+            new ArrayDenormalizer(),
+            $ConstraintViolationListNormalizer,
+            new DataUriNormalizer(new MimeTypes()),
+            new DateIntervalNormalizer(),
+            new DateTimeNormalizer(),
+            new DateTimeZoneNormalizer(),
+            new FormErrorNormalizer(),
+            new JsonSerializableNormalizer(null, null),
+            new MimeMessageNormalizer($PropertyNormalizer),
+            new ObjectNormalizer($ClassMetadataFactory, $MetadataAwareNameConverter, $PropertyAccessor, $PropertyInfoExtractor, $ClassDiscriminatorFromClassMetadata),
+            new ProblemNormalizer(true),
+            $PropertyNormalizer,
+            new UidNormalizer(),
+            new UnwrappingDenormalizer($PropertyAccessor),
+        ];
 
         self::$serializer = new Serializer($normalizers, $encoders);
     }
@@ -55,7 +117,7 @@ class OurTest extends TestCase
         $wife = new DollModel('Barbie');
         $ken = new KenModel('Ken', $wife, $kids, 'Moscow');
         $result = self::$serializer->serialize($ken, 'json');
-        $this->assertJsons($result, '
+        $this->assertJsons($result, $json = '
             {
                 "wife": {
                     "id": "Barbie"
@@ -72,6 +134,28 @@ class OurTest extends TestCase
                 "id": "Ken"
             }
         ');
+
+        // step 2: checking reverse serialization
+        $kenFromJson = self::$serializer->deserialize($json, KenModel::class, 'json');
+        $this->assertEquals($ken, $kenFromJson);
+
+        // step 3: understanding json_decode
+        $object = json_decode($json);
+        $expected = new stdClass();
+        $expected->wife = new stdClass();
+        $expected->wife->id = 'Barbie';
+        $expected->kids = [];
+        $expected->kids[0] = new stdClass();
+        $expected->kids[0]->id = 'Todd';
+        $expected->kids[1] = new stdClass();
+        $expected->kids[1]->id = 'Stacie';
+        $expected->city = 'Moscow';
+        $expected->id = 'Ken';
+        $this->assertEquals($expected, $object);
+
+        // step 4: understanding denormalize (денормализация из json завершается ошибкой, поэтому применяют deserialize)
+        $kenFromObject = self::$serializer->denormalize($object, KenModel::class);
+        $this->assertEquals($ken, $kenFromObject);
 
         return $ken;
     }
